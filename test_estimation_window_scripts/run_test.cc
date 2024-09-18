6db2970b8c2227d60e0be1e3e58d01adb11b09e8
BoriaK <boria21@gmail.com>
2024-09-18 17:37:50 +0300

    commit 23: added test results for predictive model for Uniform distribution

--- test_estimation_window_scripts/run_test.cc
+++ test_estimation_window_scripts/run_test.cc
@@ -26,39 +26,34 @@ int main ()
 {
   std::string trafficControlType = "SharedBuffer_DT"; // "SharedBuffer_DT"/"SharedBuffer_FB"
   bool accumulateStats = true; // true/false. to acumulate run statistics in a single file
-  std::string onOffTrafficMode = "Constant"; // "Constant"/"Uniform"/"Normal"
+  std::string onOffTrafficMode = "Uniform"; // "Constant"/"Uniform"/"Normal"
 
   // for local d estimatiuon 
-  double_t tau = 0.05; // Tau is Estimation Window Length [Sec]. {0.03, 0.04, 0.05}
-  double_t futurePossition = 0.75; // the placemant of the window in regards to t0 [fraction of Tau]. {0.25, 0.5, 0.75}
+  double_t tau = 0.04; // Tau is Estimation Window Length [Sec]. {0.03, 0.04, 0.05}
+  double_t futurePossition = 0.5; // the placemant of the window in regards to t0 [fraction of Tau]. {0.25, 0.5, 0.75}
   // start predictive model at: t0 - Tau*futurePossition
   // the estimation window will be: [t0 - Tau*(1-futurePossition), t0 + Tau*futurePossition]
 
   // Run option:
   // (5) single D value. Alphas are determined by Predictive Model
   // (6) multiple D values. Alphas are determined by Predictive Model
-
+  
+  // this file is for debug, to trace the sequence of all estimated d values during the run
+  // it needs to be erased before the simulation starts
+  std::remove("Estimated_D_Values.dat");
+  
   int runOption = 6; // [5, 6]
   switch (runOption)
   {
     case 5:  // single D value. Alphas are determined by Predictive Model
-    {
-      // this file is for debug, to trace the sequence of all estimated d values during the run
-      // it needs to be erased before the simulation starts
-      std::remove("Estimated_D_Values.dat");
-      
+    { 
       // select a specific d value:
       double_t miceElephantProb = 0.3; // d (- [0.1, 0.9] the probability to generate mice compared to elephant packets.
-      // viaMQueuesPredictive2ToS ("SharedBuffer_PredictiveFB", onOffTrafficMode, miceElephantProb, accumulateStats); // not in use
       viaMQueuesPredictive5ToS_v2 (trafficControlType, onOffTrafficMode, miceElephantProb, accumulateStats, futurePossition, tau);
       break;
     }
     case 6:  // multiple D values. Alphas are determined by Predictive Model
     {
-      // this file is for debug, to trace the sequence of all estimated d values during the run
-      // it needs to be erased before the simulation starts
-      std::remove("Estimated_D_Values.dat");
-
       // set an array of tested d values:
       std::double_t miceElephantProb_array[] = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9};
       // Calculate the number of elements in the array
